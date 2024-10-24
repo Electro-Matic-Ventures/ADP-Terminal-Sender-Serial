1df5cf5c2c6271bb5e8dbd0cdf78ec447c3ba264
John Machado <jrmachado@electro-matic.com>
2024-10-24 12:03:40 -0400

    ready for distro

---
+++ adp-terminal-sender.py
@@ -0,0 +1,57 @@
+import socket
+import argparse
+import re
+import time
+from datetime import datetime
+
+def parse_message(message):
+    parts = re.split(r'(\\[xX][0-9A-Fa-f]{2})', message)
+    parts = parts[1:-1]
+    parsed_message = b''
+    for part in parts:
+        if part.startswith('\\x') or part.startswith('\\X'):
+            parsed_message += bytes.fromhex(part[2:])
+        else:
+            parsed_message += part.encode('utf-8')
+    return parsed_message
+
+def send_tcpip(ip, port, parsed_message):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, port))
+    s.sendall(parsed_message)
+    s.close()
+    print(f'{datetime.now()} -- sent -- {parsed_message}')
+
+def send_from_input(ip, port, message):
+    parsed_message = parse_message(message)
+    send_tcpip(ip, port, parsed_message)
+
+def send_from_file(ip, port, filename, interval):
+    with open(filename, 'r') as file:
+        for line in file:
+            parsed_message = parse_message(line.strip())
+            send_tcpip(ip, port, parsed_message)
+            time.sleep(interval)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Send a TCP message')
+    parser.add_argument('--ip', type=str, help="Target IP address")
+    parser.add_argument('--port', type=int, help="Target port number")
+    parser.add_argument('-t', '--text', type=str, help="Message in format \\x01\\xF7")
+    parser.add_argument('-f', '--file', type=str, help="File containing messages line by line")
+    parser.add_argument('-i', '--interval', type=float, help="Interval between messages in seconds")
+    args = parser.parse_args()
+
+    if not args.ip:
+        parser.error("Target IP address (--ip) must be provided")
+    if not args.port:
+        parser.error("Target port number (--port) must be provided")
+
+    if args.text:
+        send_from_input(args.ip, args.port, args.text)
+    elif args.file:
+        if not args.interval:
+            parser.error("--interval must be provided when using --file")
+        send_from_file(args.ip, args.port, args.file, args.interval)
+    else:
+        parser.error("Either -t/--text or -f/--file must be provided")
