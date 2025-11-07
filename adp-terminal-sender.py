fcad615b9104c1c93e5a6ff5d6daef0c39f2c1c4
John Machado <jrmachado@electro-matic.com>
2025-11-07 09:12:51 -0500

    added serial sender hard code

--- adp-terminal-sender.py
+++ adp-terminal-sender.py
@@ -4,6 +4,8 @@ import re
 from random import randint
 import time
 from datetime import datetime
+import serial
+
 
 def parse_message(message):
     parts = re.split(r'(\\[xX][0-9A-Fa-f]{2})', message)
@@ -23,9 +25,25 @@ def send_tcpip(ip, port, parsed_message):
     s.close()
     print(f'{datetime.now()} -- sent -- {parsed_message}')
 
+def send_serial(parsed_message):
+        a="COM3"
+        b=9600
+        c=serial.EIGHTBITS
+        d=serial.PARITY_NONE
+        e=serial.STOPBITS_ONE
+        f=1.0
+        try:
+            with serial.Serial(port=a,baudrate=b,bytesize=c,parity=d,stopbits=e,timeout=f) as s:
+                s.write(parsed_message)
+                s.flush()
+                return f"message sent: {parsed_message}"
+        except Exception as e:
+            return f"Failed to send message: {e}"
+
 def send_from_input(ip, port, message):
     parsed_message = parse_message(message)
-    send_tcpip(ip, port, parsed_message)
+    # send_tcpip(ip, port, parsed_message)
+    send_serial(parsed_message)
 
 def send_from_file(ip, port, filename, interval):
     with open(filename, 'r') as file:
@@ -33,8 +51,9 @@ def send_from_file(ip, port, filename, interval):
     while True:
         line_number = randint(0,len(lines)-1)
         line = lines[line_number]
-        parsed = parse_message(line)
-        send_tcpip(ip, port, parsed)
+        parsed_message = parse_message(line)
+        # send_tcpip(ip, port, parsed_message)
+        send_serial(parsed_message)
         time.sleep(interval)
 
 if __name__ == "__main__":
